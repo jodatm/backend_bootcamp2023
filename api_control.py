eb5ccdd03c3b17af67e00acefd30f267c54ac2bf
jodatm <joshuadtm@gmail.com>
2023-08-25 20:20:44 -0500

    paso 2 commit

--- api_control.py
+++ api_control.py
@@ -12,7 +12,36 @@ createListCommand = listCtrlr.model('Create List command', {
     'title': fields.String(required=True, description='list description'),    
 })
 
+updateListCommand = listCtrlr.model('Update List command', {
+    'title': fields.String(required=True, description='list description'),    
+})
+
 listDto = listCtrlr.model('List DTO', {
     'id': fields.Integer(description='Id of List'),
     'title': fields.String(description='List details')    
+})
+
+taskCtrlr = Namespace(
+    'task', path="/task", description='Task API Controller')
+
+createTaskCommand = taskCtrlr.model('Create task command', {
+    'value': fields.String(required=True, description='task description'),
+    'order': fields.Integer(required=True, description='priority of task'),
+    'completed': fields.Boolean(required=True, description='task completion'),
+    'list_id': fields.Integer(required=True, description='Task must be assigned to a List')
+})
+
+updateTaskCommand = taskCtrlr.model('Update Task command', {
+    'value': fields.String(required=True, description='task description'),
+    'order': fields.Integer(required=True, description='priority of task'),
+    'completed': fields.Boolean(required=True, description='task completion'),
+    'list_id': fields.Integer(required=True, description='Task must be assigned to a List')
+})
+
+taskDto = taskCtrlr.model('Task DTO', {
+    'id': fields.Integer(description='Id of Task'),
+    'value': fields.String(description='Task details'),
+    'order': fields.Integer(description='Task order'),
+    'completed': fields.Boolean(required=True),
+    'list_id': fields.Integer(description='Task must be assigned to a List')
 })
--- models.py
+++ models.py
@@ -26,3 +26,28 @@ class CheckListSchema(ma.SQLAlchemyAutoSchema):
 
 check_list_schema = CheckListSchema()
 many_check_list_schema = CheckListSchema(many=True)
+
+class Task(db.Model):
+    __tablename__ = "task"
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String(50))
+    order = db.Column(db.Integer)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    completed = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('checklist.id', ondelete="CASCADE"))
+
+    def __repr__(self):
+        return f'<Task {self.value}>'
+    
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
+        load_instance = True
+        sqla_session = db.session
+        include_fk = True
+
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
